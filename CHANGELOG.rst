cdcc2bb11b21dc2a52b36345cbfcc174dfa9c1b4
ferrarini09@gmail.com <ferrarini09@gmail.com>
2021-05-07 12:46:44 +0200

    speech node

---
+++ CHANGELOG.rst
@@ -0,0 +1,11 @@
+^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+Changelog for package speech
+^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+
+0.1.1 (2018-09-14)
+------------------
+* Add delay functionality between end of speech and sending /robot_face/talking_finished
+
+0.1.0 (2018-07-12)
+------------------
+* First formal release of the package
---
+++ include/speech/speech_node.h
@@ -0,0 +1,43 @@
+#ifndef SPEECH_NODE_H_
+#define SPEECH_NODE_H_
+/* Speech Node. Either speaks the given text or plays a given wav file. Publishes message once taling finished
+ */
+#include <string>
+#include <ros/ros.h>
+#include <speech/voice.h>
+#include <dynamic_reconfigure/server.h>
+#include <speech/SpeechConfig.h>
+
+class SpeechNode
+{
+public:
+    SpeechNode();
+    
+    // This callback is for when the dynamic configuration parameters change
+    void reconfCallback(speech::SpeechConfig &config, uint32_t level);
+    // Check if we should delay before sending message on talking_finished topic
+    void speakingFinished(); 
+    
+    const double LOOP_FREQUENCY_ = 10;
+
+private:
+    ros::NodeHandle n_;
+    ros::Subscriber voice_sub_;    
+    ros::Publisher talking_finished_pub_;
+    
+    std::string language_;
+    int pitch_;
+    int bass_;
+    int treble_;
+    double vol_;
+    bool norm_;
+    
+    bool finshed_speaking_;
+    int loop_count_down_;        
+
+    // This callback is for when a voice message received
+    void voiceCallback(const speech::voice& voice);     
+};
+
+#endif // SPEECH_NODE_H_
+
